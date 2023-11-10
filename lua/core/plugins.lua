2e7880f4e5a4983a948a7813642bc5671dea4b81
Michael Gregoire <mgregoire@nvzion.com>
2023-11-10 16:48:01 -0500

    Add markdown-preview

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -58,7 +58,13 @@ local plugins = {
       -- add any options here
     },
     lazy = false,
-  }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
 
 local opts = {}
