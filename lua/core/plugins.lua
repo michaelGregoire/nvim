b8097125d9e87f8279a8f9c415b0723865718f4b
Michael Gregoire <mgregoire@nvzion.com>
2024-02-06 20:38:13 -0500

    Add Zen Mode

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -54,6 +54,14 @@ local plugins = {
       -- refer to the configuration section below
     }
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
 
   -- Tab Bar
   'romgrk/barbar.nvim',
