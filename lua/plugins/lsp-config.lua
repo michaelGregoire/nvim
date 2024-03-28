a0142686954b9c3af021b35fcc15eafaf7544665
Michael Gregoire <mgregoire@nvzion.com>
2024-03-28 12:16:12 -0400

    Add completions and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,19 +16,45 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.astro.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.eslint.setup({})
-      lspconfig.emmet_ls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.mdx_analyzer.setup({})
-      lspconfig.intelephense.setup({})
-      lspconfig.sqlls.setup({})
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.astro.setup({
+        capabilities = capabilities
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.eslint.setup({
+        capabilities = capabilities
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.marksman.setup({
+        capabilities = capabilities
+      })
+      lspconfig.mdx_analyzer.setup({
+        capabilities = capabilities
+      })
+      lspconfig.intelephense.setup({
+        capabilities = capabilities
+      })
+      lspconfig.sqlls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
